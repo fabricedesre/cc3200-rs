45f2b51380f35aabbddb3ebb1f281a55fe833444
Fabrice Desré <fabrice@desre.org>
2016-10-31 16:54:58 -0700

    Switch to a library crate with an example

--- build.sh
+++ build.sh
@@ -9,12 +9,12 @@ done
 
 TARGET=thumbv7em-none-eabi
 ELF_DIR=target/${TARGET}/${OPT}
-FIRMWARE_ELF=${ELF_DIR}/firmware
-FIRMWARE_BIN=${ELF_DIR}/firmware.bin
+FIRMWARE_ELF=${ELF_DIR}/examples/blinky
+FIRMWARE_BIN=${ELF_DIR}/examples/blinky.bin
 
 rm -f ${FIRMWARE_ELF}
 rm -f ${FIRMWARE_BIN}
 
-xargo build --target=${TARGET} "$@" && \
+xargo build --target=${TARGET} "$@" --example blinky && \
 arm-none-eabi-size ${FIRMWARE_ELF} && \
 arm-none-eabi-objcopy -O binary ${FIRMWARE_ELF} ${FIRMWARE_BIN}
--- flash.sh
+++ flash.sh
@@ -9,8 +9,8 @@ done
 
 TARGET=thumbv7em-none-eabi
 ELF_DIR=target/${TARGET}/${OPT}
-FIRMWARE_ELF=${ELF_DIR}/firmware
-FIRMWARE_BIN=${ELF_DIR}/firmware.bin
+FIRMWARE_ELF=${ELF_DIR}/examples/blinky
+FIRMWARE_BIN=${ELF_DIR}/examples/blinky.bin
 
 set -x
 cc3200tool -p /dev/ttyUSB1 --sop2 ~dtr --reset prompt write_file ${FIRMWARE_BIN} /sys/mcuimg.bin
