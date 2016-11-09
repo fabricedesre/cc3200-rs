9ffb4bff904738f3d674e63df25afe078bd0a726
Fabrice Desré <fabrice@desre.org>
2016-11-09 13:16:30 -0800

    Removing commented/#if 0 sections and using the same gcc options in both crates

--- cc3200-sys/board.c
+++ cc3200-sys/board.c
@@ -212,45 +212,6 @@ vApplicationIdleHook( void)
     //Handle Idle Hook for Profiling, Power Management etc
 }
 
-#if 0
-#define likely(x) __builtin_expect((x), 1)
-
-void *memcpy(void *dst, const void *src, size_t n) {
-    if (likely(!(((uintptr_t)dst) & 3) && !(((uintptr_t)src) & 3))) {
-        // pointers aligned
-        uint32_t *d = dst;
-        const uint32_t *s = src;
-
-        // copy words first
-        for (size_t i = (n >> 2); i; i--) {
-            *d++ = *s++;
-        }
-
-        if (n & 2) {
-            // copy half-word
-            *(uint16_t*)d = *(const uint16_t*)s;
-            d = (uint32_t*)((uint16_t*)d + 1);
-            s = (const uint32_t*)((const uint16_t*)s + 1);
-        }
-
-        if (n & 1) {
-            // copy byte
-            *((uint8_t*)d) = *((const uint8_t*)s);
-        }
-    } else {
-        // unaligned access, copy bytes
-        uint8_t *d = dst;
-        const uint8_t *s = src;
-
-        for (; n; n--) {
-            *d++ = *s++;
-        }
-    }
-
-    return dst;
-}
-#endif
-
 void *memset(void *s, int c, size_t n) {
     if (c == 0 && ((uintptr_t)s & 3) == 0) {
         // aligned store of 0
