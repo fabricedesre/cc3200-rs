4d1e8eeff6ca5d078db06f829b5b568a37bf7029
Dave Hylands <dhylands@gmail.com>
2016-11-11 19:22:11 -0800

    Make travis.sh build all of the examples.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 curl https://sh.rustup.rs -sSf -o rustup.sh
 chmod +x ./rustup.sh
@@ -7,5 +7,9 @@ export PATH=/home/travis/.cargo/bin:$PATH
 rustup default nightly
 rustup component add rust-src
 cargo install xargo
-./build.sh
-./build.sh --release
+
+for example_file in examples/*.rs; do
+    example=$(basename ${example_file/.rs/})
+    ./build.sh --example ${example}
+    ./build.sh --example ${example} --release
+done
