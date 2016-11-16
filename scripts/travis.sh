1c055ebb0a16f63957b91986da0a0d4c1d05af8b
Dave Hylands <dhylands@gmail.com>
2016-11-16 11:45:24 -0800

    Went back to rustc 1.14
    xargo doesn't seem to be building under 1.15

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -4,9 +4,13 @@ curl https://sh.rustup.rs -sSf -o rustup.sh
 chmod +x ./rustup.sh
 ./rustup.sh -y
 export PATH=/home/travis/.cargo/bin:$PATH
-rustup default nightly
+rustup override set nightly-2016-11-06
 rustup component add rust-src
 cargo install xargo
+cargo --version
+xargo --version
+rustc --version
+arm-none-eabi-gcc --version
 
 for example_file in examples/*.rs; do
     example=$(basename ${example_file/.rs/})
