5ee0dc727d5258dced88d60ce50703ca3bd27086
Dave Hylands <dhylands@gmail.com>
2016-12-14 13:08:53 -1000

    Added license headers

--- numeric_utils/tests/test_format.rs
+++ numeric_utils/tests/test_format.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at http://mozilla.org/MPL/2.0/.
+
 extern crate numeric_utils;
 
 use numeric_utils::{format_float_into, format_int_into};
