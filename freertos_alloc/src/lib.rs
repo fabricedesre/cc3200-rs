2e5b903b7dda6b943a1d62bb7dc9f4f38afcceab
Dave Hylands <dhylands@gmail.com>
2016-12-14 14:06:09 -1000

    Added license headers to *.c and *.rs files

--- freertos_alloc/src/lib.rs
+++ freertos_alloc/src/lib.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at http://mozilla.org/MPL/2.0/.
+
 // The compiler needs to be instructed that this crate is an allocator in order
 // to realize that when this is linked in another allocator like jemalloc should
 // not be linked in
